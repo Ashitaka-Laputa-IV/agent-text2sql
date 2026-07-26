dbf75814627a4e5ff5c35a0ca8a3ce69b838911a
Ashitaka-Laputa-IV <Ashitaka-Laputa-IV@outlook.com>
2026-07-26 16:20:27 +0800

    main.py: 为 agent 增加 SQLDatabaseToolkit SQL 操作工具

--- text2sql/main.py
+++ text2sql/main.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.utilities import SQLDatabase
 from deepagents import create_deep_agent
 
 load_dotenv()
@@ -9,6 +11,7 @@ load_dotenv()
 MODEL_NAME: str = os.getenv("model_name", "deepseek-v4-flash")
 MODEL_BASE_URL: str = os.getenv("model_base_url", "https://api.deepseek.com")
 MODEL_API: str = os.getenv("model_api", "")
+DB_URI: str = os.getenv("db_uri", "")
 
 model = ChatOpenAI(
     model=MODEL_NAME,
@@ -16,7 +19,12 @@ model = ChatOpenAI(
     api_key=MODEL_API,
 )
 
+db = SQLDatabase.from_uri(DB_URI)
+toolkit = SQLDatabaseToolkit(db=db, llm=model)
+tools = toolkit.get_tools()
+
 agent = create_deep_agent(
     model=model,
+    tools=tools,
     memory=["./AGENTS.md"],
 )
